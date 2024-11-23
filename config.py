70a7d5e49701409adb764f0af7a7af9b7f8e64fb
Satyamkumarjha4 <satyamjha4@gmail.com>
2024-11-23 17:34:43 +0530

    corrected config

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ EXTRA_COL = 'Unnamed: 0'
 IMP_COL_NUMERICAL = ['Current Price', 'Original Price',
                      'Rating', 'Number OF Ratings', 'Display Size']
 IMP_COL_CATOGORICAL = ['Brand', 'Model Name', 'Dial Shape', 'Strap Material']
-CLEAN_FILE_PATH = './data/clean.csv'
+CLEAN_FILE_PATH = './data/clean_data.csv'
 MODEL_SAVE_NAME = './models/model.pkl'
